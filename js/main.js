80b7ef2eaab3b594a8b3a9ac65680ada87e587a4
Dejorden <d.moerman@hotmail.nl>
2023-08-25 14:42:26 +0200

    write logic for article based on data length

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
+{/* <article class="cleanCard">
+    <header class="cleanCard__header">
+        <h1 class="cleanCard__heading">Elke dag</h1>
+    </header>
+    <ul class="cleanCard__activities">
+        <li class="cleanCard_activity cleanCard_activity--done">Eten</li>
+        <li class="cleanCard_activity">Trainen</li>
+        <li class="cleanCard_activity">Programmeren</li>
+    </ul>
+</article> */}
+
+
 const renderCards = (data) => {
-    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+        //Maak een article viajavascript
+        let article = document.createElement("article");
+        //Geef dezede class cleanCard
+        article.classList = "cleanCard";
+        //Stop deze in de body
+        document.querySelector("body").appendChild(article);
+    }
 }
 
 fetch("../data/cards.json").then(
     response => response.json()
 ).then(
-    data => consolerenderCards(data)
+    data => renderCards(data)
 )
